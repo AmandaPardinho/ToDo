18695357ed0437b7340f5c234d5f5947e770950c
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-07-24 14:41:22 -0300

    Criação do pojeto ToDo e ligação com o banco de dados

---
+++ ToDo/Data/TodoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ToDo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.Data
+{
+    //contexto de dados => representação do banco em memória (mapa do banco de dados)
+    public class TodoDbContext: DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+
+        }
+
+        //representa a tabela no banco de dados
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
